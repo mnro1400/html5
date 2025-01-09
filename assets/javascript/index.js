2a6c244e5d7322cfe0659a6c1c085fb8c20ecc7b
mahdi <mahdi.pc.2024.py@gmail.com>
2025-01-09 05:57:20 -0800

    part #6

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,3 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
 });
+const adduserbutton = document.getElementById('add_user-botton')
+adduserbutton.addEventListener('click', () {
+        document.querySelectorA
+    }
+
+)
